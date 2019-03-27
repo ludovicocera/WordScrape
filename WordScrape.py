de50dfad44ecbb7502272e86ad24e06e5493659a
Ludovico Cera <ludocera@gmail.com>
2019-03-27 09:46:21 +0100

    Implemented a new, more consistent, parsing method

--- WordScrape.py
+++ WordScrape.py
@@ -2,6 +2,7 @@ import bs4
 import requests
 import re
 import collections
+import string
 from bs4 import Comment
 from urllib.parse import urlparse
 from urllib.request import urlopen, URLError
@@ -24,9 +25,9 @@ def visit(url):
 
 
 def scrapeWords(url):
-    r = requests.get(url)
+    html = urlopen(url).read()
 
-    soup = bs4.BeautifulSoup(r.text, "html.parser")
+    soup = bs4.BeautifulSoup(html, "html.parser")
 
     for script in soup(["script", "style"]):
         script.extract()
@@ -36,11 +37,15 @@ def scrapeWords(url):
         if isinstance(element, Comment):
             element.extract()
 
-    cleanedText = re.sub('[^a-zA-Z0-9 ]+', ' ', soup.text.lower())
+    wordList = soup.text.split()
 
-    wordList = cleanedText.split()
+    cleanedText = list()
 
-    return wordList
+    for word in wordList:
+        cleanedText.append(word.lower().translate(
+            str.maketrans('', '', string.punctuation)))
+
+    return cleanedText
 
 
 def collectLinks(url):
@@ -92,6 +97,21 @@ def validateUrl(url):
         return False
 
 
+def tag_visible(element):
+    if element.parent.name in ['style', 'script', 'head', 'title', 'meta', '[document]']:
+        return False
+    if isinstance(element, Comment):
+        return False
+    return True
+
+
+def text_from_html(body):
+    soup = bs4.BeautifulSoup(body, 'html.parser')
+    texts = soup.findAll(text=True)
+    visible_texts = filter(tag_visible, texts)
+    return u" ".join(t.strip() for t in visible_texts)
+
+
 visited = set()
 
 totalWords = list()
@@ -106,5 +126,5 @@ print(visited)
 
 for element in totalWords:
     wordsDict[element] = totalWords.count(element)
-    
-print(sorted(wordsDict.items(), key=lambda kv: kv[1], reverse = True))
+
+print(sorted(wordsDict.items(), key=lambda kv: kv[1], reverse=True))
